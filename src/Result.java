ec1db4d0b5466036293a6847d64bbb0e6b45aa56
Valdemar <56624331+Valdemar99@users.noreply.github.com>
2019-12-10 14:38:32 +0100

    Koppling till klasser
    Kopplade till klasserna Student och WrittenExam

--- src/Result.java
+++ src/Result.java
@@ -2,6 +2,8 @@
 public class Result {
 	private int result;
 	private char letterGrade;
+	private WrittenExam exam;
+	private Student student;
 	
 	public int getResult() {
 		return result;
@@ -14,6 +16,18 @@ public class Result {
 	}
 	public void setLetterGrade(char letterGrade) {
 		this.letterGrade = letterGrade;
+	}
+	public WrittenExam getExam() {
+		return exam;
+	}
+	public void setExam(WrittenExam exam) {
+		this.exam = exam;
+	}
+	public Student getStudent() {
+		return student;
+	}
+	public void setStudent(Student student) {
+		this.student = student;
 	}
 	
 	
