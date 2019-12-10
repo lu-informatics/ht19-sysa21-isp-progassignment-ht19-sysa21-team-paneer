c13dc7d7b680b96f4b9bd25c3ce06fc6d21bd217
Valdemar <56624331+Valdemar99@users.noreply.github.com>
2019-12-10 14:27:27 +0100

    Skapade Result-klassen
    Skapade den med attribut och åtkomstmetoder

---
+++ src/Result.java
@@ -0,0 +1,22 @@
+
+public class Result {
+	private int result;
+	private char letterGrade;
+	
+	public int getResult() {
+		return result;
+	}
+	public void setResult(int result) {
+		this.result = result;
+	}
+	public char getLetterGrade() {
+		return letterGrade;
+	}
+	public void setLetterGrade(char letterGrade) {
+		this.letterGrade = letterGrade;
+	}
+	
+	
+	
+
+}
