0c2c99c66ccd3022ffba39bc416ce35f62aef1f9
VickyYaoMing <56544221+VickyYaoMing@users.noreply.github.com>
2019-12-12 12:36:31 +0100

    Student
    Klasserna för studentregister och student

---
+++ src/StudentRegister.java
@@ -0,0 +1,37 @@
+
+import java.util.HashMap;
+
+public class StudentRegister {
+
+	private HashMap<String, Student> studenter = new HashMap<String, Student>();
+
+	public HashMap<String, Student> getStudenter() {
+		return studenter;
+	}
+
+	public void setStudenter(HashMap<String, Student> studenter) {
+		this.studenter = studenter;
+	}
+
+	public void addStudent(Student studentID) {
+		studenter.put(studentID.getStudentId(), studentID);
+	}
+
+	public Student removeStudent(String studentID) {
+		return studenter.remove(studentID);
+	}
+
+	public Student findStudent(String studentID) {
+		return studenter.get(studentID);
+
+	}
+
+	public Student editStudent(String studentID, String name) {
+		Student s = studenter.get(studentID);
+		if (s != null) {
+			s.setName(name);
+		}
+		return s;
+	}
+
+}
